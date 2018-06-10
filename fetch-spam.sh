0cbf89d68e48f373d85d4d4cc5a27950bf8faac7
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-06-10 23:16:43 +0300

    re-add old spam-fetching Bash script, dropping the usage of the `-H` ("span hosts") switch for wget

---
+++ fetch-spam.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+spam_topic_page_url_template='http://forum.animes-bg.com/viewtopic.php?f=18&t=75541&start='
+spam_step=15
+spam_target_directory=.
+
+while getopts :p:s:t: option
+do
+    case $option in
+        p)
+            spam_topic_page_url_template=$OPTARG
+            ;;
+
+        s)
+            spam_step=$OPTARG
+            ;;
+
+        t)
+            spam_target_directory=$OPTARG
+            ;;
+    esac
+done
+
+shift $((OPTIND - 1))
+
+spam_max_page_number=$1
+[[ -z $spam_max_page_number ]] && exit 1
+shift
+
+for spam_page_number in $(seq 1 $spam_max_page_number)
+do
+    spam_page_target_directory="${spam_target_directory}/${spam_page_number}"
+    mkdir -p "${spam_page_target_directory}"
+    [[ $? -ne 0 ]] && continue
+    spam_offset=$(( spam_step * (spam_page_number - 1) ))
+    wget -bEkKp -o "${spam_page_target_directory}/wget-log" -P "${spam_page_target_directory}" "${spam_topic_page_url_template}${spam_offset}" > /dev/null
+done

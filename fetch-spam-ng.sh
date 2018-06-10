36ffb2f9738b99bbbe28368299da552e8d1d69e5
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-06-11 01:28:51 +0300

    refactor: rename some variables

--- fetch-spam-ng.sh
+++ fetch-spam-ng.sh
@@ -4,8 +4,8 @@ script_name=$0
 
 max_job_count=$(grep -c ^processor /proc/cpuinfo)
 spam_topic_page_url_template='http://forum.animes-bg.com/viewtopic.php?f=18&t=75541&start='
-spam_step=15
-spam_target_directory=.
+spam_topic_posts_step=15
+target_directory=.
 
 while getopts :Hj:p:s:t:v option
 do
@@ -23,11 +23,11 @@ do
             ;;
 
         s)
-            spam_step=$OPTARG
+            spam_topic_posts_step=$OPTARG
             ;;
 
         t)
-            spam_target_directory=$OPTARG
+            target_directory=$OPTARG
             ;;
 
         v)
@@ -38,8 +38,8 @@ done
 
 shift $((OPTIND - 1))
 
-spam_max_page_number=$1
-if [[ -z $spam_max_page_number ]]
+spam_topic_max_page_number=$1
+if [[ -z $spam_topic_max_page_number ]]
 then
     echo "${script_name}: no maximum page number specified"
     exit 1
@@ -52,36 +52,36 @@ function decrement_job_count
     [[ -n $is_verbose_mode ]] && echo "Job with pid $! has just finished its execution (${job_count} more jobs remaining)." >&2
 }
 
-function wget_spam_page_and_notify
+function wget_spam_topic_page_and_notify
 {
-    spam_page_url=${spam_topic_page_url_template}${spam_offset}
+    spam_topic_page_url=${spam_topic_page_url_template}${spam_topic_posts_offset}
     if [[ -n $is_verbose_mode ]]
     then
-        echo "Starting the fetching of page ${spam_page_number} into directory ${spam_page_target_directory}..." >&2
-        echo "URL: ${spam_page_url}" >&2
+        echo "Starting the fetching of page ${spam_topic_page_number} into directory ${spam_topic_page_target_directory}..." >&2
+        echo "URL: ${spam_topic_page_url}" >&2
     fi
-    wget -EkKp ${span_hosts} -o "${spam_page_target_directory}/wget-log" -P "${spam_page_target_directory}" "${spam_page_url}"
-    [[ -n $is_verbose_mode ]] && echo "Finished the fetching of page ${spam_page_number}." >&2
+    wget -EkKp ${span_hosts} -o "${spam_topic_page_target_directory}/wget-log" -P "${spam_topic_page_target_directory}" "${spam_topic_page_url}"
+    [[ -n $is_verbose_mode ]] && echo "Finished the fetching of page ${spam_topic_page_number}." >&2
     kill -USR1 $$
 }
 
 job_count=0
 trap decrement_job_count SIGUSR1
 
-for spam_page_number in $(seq 1 $spam_max_page_number)
+for spam_topic_page_number in $(seq 1 $spam_topic_max_page_number)
 do
     while [[ $job_count -ge $max_job_count ]]
     do
         wait -n
     done
 
-    spam_page_target_directory="${spam_target_directory}/${spam_page_number}"
-    mkdir -p "${spam_page_target_directory}"
+    spam_topic_page_target_directory="${target_directory}/${spam_topic_page_number}"
+    mkdir -p "${spam_topic_page_target_directory}"
     [[ $? -ne 0 ]] && continue
-    spam_offset=$(( spam_step * (spam_page_number - 1) ))
+    spam_topic_posts_offset=$(( spam_topic_posts_step * (spam_topic_page_number - 1) ))
     job_count=$((job_count + 1))
     [[ -n $is_verbose_mode ]] && echo "Starting a new background job (${job_count} jobs total)." >&2
-    wget_spam_page_and_notify &
+    wget_spam_topic_page_and_notify &
 done
 
 while ! wait

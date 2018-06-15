df0d3c23bb284116379e3525da4ee0d36cb280b6
Radoslav Georgiev <rgeorgiev583@gmail.com>
2018-06-15 22:47:21 +0300

    fix output handling: make all errors and warnings appear on STDERR and normal program operation messages appear on STDOUT

--- fetch-forum-topic-ng.sh
+++ fetch-forum-topic-ng.sh
@@ -40,7 +40,7 @@ shift $((OPTIND - 1))
 forum_topic_page_url_template=$1
 if [[ -z $forum_topic_page_url_template ]]
 then
-    echo "${script_name}: no base URL for forum topic pages specified"
+    echo "${script_name}: no base URL for forum topic pages specified" >&2
     exit 1
 fi
 shift
@@ -54,14 +54,14 @@ fi
 
 if [[ -z $forum_topic_page_numbers ]]
 then
-    echo "${script_name}: no range specified of forum topic pages to download"
+    echo "${script_name}: no range specified of forum topic pages to download" >&2
     exit 2
 fi
 
 function decrement_job_count
 {
     job_count=$((job_count - 1))
-    [[ -n $is_verbose_mode ]] && echo "Job with pid $! has just finished its execution (${job_count} more jobs remaining)." >&2
+    [[ -n $is_verbose_mode ]] && echo "Job with pid $! has just finished its execution (${job_count} more jobs remaining)."
 }
 
 function wget_forum_topic_page_and_notify
@@ -69,11 +69,11 @@ function wget_forum_topic_page_and_notify
     forum_topic_page_url=${forum_topic_page_url_template}${forum_topic_posts_offset}
     if [[ -n $is_verbose_mode ]]
     then
-        echo "Starting the fetching of page ${forum_topic_page_number} into directory ${forum_topic_page_target_directory}..." >&2
-        echo "URL: ${forum_topic_page_url}" >&2
+        echo "Starting the fetching of page ${forum_topic_page_number} into directory ${forum_topic_page_target_directory}..."
+        echo "URL: ${forum_topic_page_url}"
     fi
     wget -EkKp ${span_hosts} -o "${forum_topic_page_target_directory}/wget-log" -P "${forum_topic_page_target_directory}" "${forum_topic_page_url}"
-    [[ -n $is_verbose_mode ]] && echo "Finished the fetching of page ${forum_topic_page_number}." >&2
+    [[ -n $is_verbose_mode ]] && echo "Finished the fetching of page ${forum_topic_page_number}."
     kill -USR1 $$
 }
 
@@ -92,7 +92,7 @@ do
     [[ $? -ne 0 ]] && continue
     forum_topic_posts_offset=$(( forum_topic_posts_step * (forum_topic_page_number - 1) ))
     job_count=$((job_count + 1))
-    [[ -n $is_verbose_mode ]] && echo "Starting a new background job (${job_count} jobs total)." >&2
+    [[ -n $is_verbose_mode ]] && echo "Starting a new background job (${job_count} jobs total)."
     wget_forum_topic_page_and_notify &
 done
 
